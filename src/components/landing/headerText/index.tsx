d70fef726895a08cd0d012ddea5e6e286ba81965
JerzyDevelopment <jerzyboguckidevelopment@gmail.com>
2023-06-08 21:47:13 +0100

    change to header text

--- src/components/landing/headerText/index.tsx
+++ src/components/landing/headerText/index.tsx
@@ -2,7 +2,9 @@ const HeaderText = () => {
   return (
     <div className="container text-shadow">
       <h1>SpotifyMe</h1>
-      <h3 className="fs-4">Simply sign in and check your listening stats!</h3>
+      <h3 className="fs-4">
+        Simply sign in and check your listening stats of the last 6 months!
+      </h3>
     </div>
   );
 };
